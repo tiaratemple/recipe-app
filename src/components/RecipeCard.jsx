254b6561a00dc55bfc2a48284880456c2076b519
Tiara Temple <tmtemple7@gmail.com>
2022-11-23 16:44:48 -0600

    static homepage complete with searchbar, cards and header/footer

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './RecipeCard.css'
 
 const RecipeCard = () => {
   return (
-    <div>RecipeCard</div>
+    <div className='recipe-card-section'>
+      <div className="recipe-card-container">
+        <img className='recipe-card-img' src="https://omgchocolatedesserts.com/wp-content/uploads/2017/07/Triple-Chocolate-Mouse-Cake-Minis-featured-new.jpg">   
+        </img>
+        <h1 className='recipe-card-title'>Triple Chocolate Mousse Cake</h1>
+        <button className="blue-btn">See More</button>
+      </div>
+
+      <div className="recipe-card-container">
+        <img className='recipe-card-img' src="https://simplyhomecooked.com/wp-content/uploads/2020/02/ribeye-steak-in-the-oven-4-683x1024.jpg">   
+        </img>
+        <h1 className='recipe-card-title'>Rosemary Seared Steak</h1>
+        <button className="blue-btn">See More</button>
+      </div>
+
+      <div className="recipe-card-container">
+        <img className='recipe-card-img' src="https://www.willcookforsmiles.com/wp-content/uploads/2022/04/Dirty-Martini-683x1024.jpg">   
+        </img>
+        <h1 className='recipe-card-title'>Dirty Martini</h1>
+        <button className="blue-btn">See More</button>
+      </div>
+    </div>
   )
 }
 export default RecipeCard;
--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,11 +1,18 @@
-import axios from 'axios'
-import { useEffect } from 'react'
-import AdBanner from './AdBanner'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import RecipeCard from '../RecipeCard';
+import AdBanner from './AdBanner';
+import { BsSearch } from "react-icons/bs";
 
 const HomeScreen = () => {  
+  const [recipes, setRecipes] = useState([])
+  const url = "https://recipes.devmountain.com"
+  const [search, setSearch] = useState("");
+
+  
   const getRecipes = () => {
     axios
-      .get("https://recipes.devmountain.com/recipes")
+      .get(`${url}/recipes`)
       .then((res) => {
         setRecipes(res.data)
         console.log(res.data)
@@ -16,10 +23,31 @@ const HomeScreen = () => {
     getRecipes()
   }, [])
 
+  const recipeDisplay = recipes
+    .filter((recipe, index) => {
+        let title = recipe.recipe_name.toLowerCase()
+        let searchParams = search.toLowerCase()
+        return title.includes(searchParams)
+    })
+    .map((recipe, index) => {
+        return <RecipeCard recipe={recipe}/>
+    })
+
   return (
     <div>
       <AdBanner />
-      {/* Much code from Part 2 will be placed around here. Do your best! */}
+        <div className='search-container'>
+          <span className='search-bar'>
+            <BsSearch size="1em" color="#ff8400" />       
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search for a Recipe"
+            />
+          </span>
+        </div>
+      <RecipeCard />
     </div>
   )
 }
