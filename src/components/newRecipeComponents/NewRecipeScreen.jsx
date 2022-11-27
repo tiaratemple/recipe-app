7b5c8f73c1a9fcb0e0e34c5b89d01c9ac40034cc
Tiara Temple <tmtemple7@gmail.com>
2022-11-27 16:19:33 -0600

    form added

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,10 +1,91 @@
 import React from "react";
+import "../newRecipeComponents/NewRecipeScreen.css";
 
 const NewRecipeScreen = () => {
   return (
-    <section>
-      <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
+    <section className="form-container">
+      <h1 className="form-h1-styles">Tell us about your Recipe!</h1>
+      <form>
+        <div className="form-row-top">
+          <input
+            type="text"
+            value=""
+            name="recipeName"
+            placeholder="Name"
+          ></input>
+          <input
+            type="text"
+            value=""
+            name="imageURL"
+            placeholder="Image URL"
+          ></input>
+        </div>
+        <div className="form-radio-row">
+          <label>
+            <input type="radio" value="" name="cook"></input>
+            Cook
+          </label>
+          <label>
+            <input type="radio" value="" name="bake"></input>
+            Bake
+          </label>
+          <label>
+            <input type="radio" value="" name="drink"></input>
+            Drink
+          </label>
+        </div>
+        <div className="form-row-3">
+          <input
+            type="text"
+            value=""
+            name="prepTime"
+            placeholder="Prep Time"
+          ></input>
+          <input
+            type="text"
+            value=""
+            name="cookTime"
+            placeholder="Cook Time"
+          ></input>
+          <input
+            type="number"
+            value=""
+            name="serves"
+            placeholder="Serves"
+          ></input>
+        </div>
+        <div className="form-row-4">
+          <div className="form-column">
+            <input
+              type="text"
+              value=""
+              name="ingredient"
+              placeholder="Ingredient"
+            ></input>
+            <input
+              type="number"
+              value=""
+              name="quantity"
+              placeholder="Quantity"
+            ></input>
+          </div>
+          <div className="form-column">
+            <ul>
+              <li>4 eggs</li>
+              <li>1 box of cake</li>
+            </ul>
+          </div>
+        </div>
+        <div className="btn-container">
+          <button className="add-btn">Add Another</button>
+        </div>
+        <div className="text-area-container">
+          <textarea name="instructions">What are the instructions?</textarea>
+        </div>
+        <div className="btn-container">
+          <button className="save-btn">Save</button>
+        </div>
+      </form>
     </section>
   );
 };
